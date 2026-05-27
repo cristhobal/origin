64383418ac4c26816cf4884d516581916e24f452
cristhobal <cristhobalcanalesespinoza@gmail.com>
2026-05-27 18:38:13 -0400

    Update github.service.ts

--- src/features/projects/services/github.service.ts
+++ src/features/projects/services/github.service.ts
@@ -106,6 +106,7 @@ const REPO_LOGOS: Record<string, string> = {
 const REPO_PREVIEWS: Record<string, string> = {
   vault: "https://i.ibb.co/BFxJHhH/vault.png",
   diagnost: "https://i.ibb.co/v6BhRdpv/diagnost.png",
+  chrono: "https://i.ibb.co/TMSjJbp7/chrono.png",
   // "mi-repo-privado": "https://cdn.example.com/preview.jpg",
 };
 
